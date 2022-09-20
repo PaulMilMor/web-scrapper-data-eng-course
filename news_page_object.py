41b4d5ba5d4599668b80fa7f2771e37b19ff10a1
Paul Millanes <pauljmm99@gmail.com>
2022-09-19 22:49:07 -0700

    Get news links from news sites

---
+++ news_page_object.py
@@ -0,0 +1,39 @@
+from email import header
+import bs4
+import requests
+
+from common import config
+
+
+class HomePage:
+
+    def __init__(self, news_site_uid, url):
+        self._config = config()['news_sites'][news_site_uid]
+        self._queries = self._config['queries']
+        self._html = None
+
+        self._visit(self._config['url'])
+
+
+    @property
+    def article_links(self):
+        link_list = []
+        for link in self._select(self._queries['homepage_article_links']):
+            if link and link.has_attr('href'):
+                link_list.append(link)
+        return set(link['href'] for link in link_list)
+
+
+    def _select(self, query_string):
+        nodes = self._html.select(query_string)
+
+        if not nodes:
+            return None
+        return nodes
+
+
+    def _visit(self, url):
+        headers = {'User-Agent': 'Chrome/51.0.2704.103 Safari/537.36'}
+        response = requests.get(url, headers=headers)
+        response.raise_for_status()
+        self._html = bs4.BeautifulSoup(response.text, 'html.parser')
